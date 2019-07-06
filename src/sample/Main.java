faf6e621ee268a3de43e28765b4aa0267a90fc3c
Yulia357 <52542651+Yulia357@users.noreply.github.com>
2019-07-06 16:40:42 +0500

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Main {
+
+       public static void main (String [] args) {
+           SimpleGUI app = new SimpleGUI();
+           app.setVisible(true);
+       }
+}
